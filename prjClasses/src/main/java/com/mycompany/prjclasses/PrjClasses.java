2451cecbcc24a92968c4e610b348b60ab1ef6e05
LwaziM07 <ST10434465@vcconnect.edu.za>
2025-03-07 11:55:53 +0200

    Updated Project
    Added classes

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,12 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Greetings peasant! Look at my work and cringe at your inferiority!");
+        
+            newClass nc = new newClass(); //Created a new class
+                                        // Used a variable "nc" to communicate to that class.
+            
+            nc.helloClass();
+            
+            System.out.println(nc.str2ndClassVar);
     }
 }
